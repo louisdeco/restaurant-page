b337f25202563c6a2cd1e5c503e37e5e71e1bb90
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-06-12 14:12:34 +0400

    Add webpack with HTML, JS, CSS,

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+function test () {
+    const element = document.createElement("div");
+    element.textContent = "Hello, World!";
+
+    element.classList.add("test")
+
+    return element
+}
+
+document.body.appendChild(test());
