6340eca6181b20ecc0278ee2227ad3f3ddd91955
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-06-14 16:21:51 +0400

    Add dynamic rendering of the about page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import "./styles.css";
 import homePage from "./home-page";
+import aboutPage from "./about-page";
 
 const content = document.querySelector(".content");
+const aboutButton = document.querySelector(".about");
+const homeButton = document.querySelector(".home");
 
 homePage(content);
+
+homeButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    homePage(content);
+})
+
+aboutButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    aboutPage(content);
+})
+
